fda33b3fbb4975a5d6e1d094e2276ebe808e7a27
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-12-10 22:09:43 +0000

    Replace the sprite dump with actual canvas content

--- map.js
+++ map.js
@@ -4,10 +4,13 @@ class MapReader {
      *
      * @param {string} input_ref
      * @param {string} output_ref
+     * @param {string} map_ref
      */
-    constructor(input_ref, output_ref) {
+    constructor(input_ref, output_ref, map_ref) {
         /** @type {HTMLInputElement} */
         this.in = document.querySelector(input_ref)
+        /** @type {HTMLCanvasElement} */
+        this.mapOut = document.querySelector(map_ref)
         this.out = document.querySelector(output_ref)
         /** @type {ArrayBuffer} */
         this.data = null
@@ -97,27 +100,48 @@ class MapReader {
             })
             content += "\n"
         }
-        let sprite16 = (o) => {
+        let ctx = this.mapOut.getContext("2d")
+        let bit = (n, j) => {
+            return (n >> (7-j)) & 1
+        }
+        let sprite16 = (o, p) => {
+            let d = ctx.createImageData(16, 16)
             for(let i = 0; i < 2; i++) {
                 let x = new Uint8Array(this.data, o + i * 16, 16)
                 for(let r = 0; r < 8; r++) {
-                    let s = x[r].toString(2).padStart(8, 0) + x[r+8].toString(2).padStart(8, 0)
-                    content += s.replace(/0/g, "  ").replace(/1/g, "##") + "\n"
+                    for(let j = 0; j < 8; j++) {
+                        d.data[i * 512 + r * 64 + j * 4 + 0] = 255 * bit(x[r], j)
+                        d.data[i * 512 + r * 64 + j * 4 + 1] = 255 * bit(x[r], j)
+                        d.data[i * 512 + r * 64 + j * 4 + 2] = 255 * bit(x[r], j)
+                        d.data[i * 512 + r * 64 + j * 4 + 3] = 255
+                        d.data[i * 512 + r * 64 + 32 + j * 4 + 0] = 255 * bit(x[r + 8], j)
+                        d.data[i * 512 + r * 64 + 32 + j * 4 + 1] = 255 * bit(x[r + 8], j)
+                        d.data[i * 512 + r * 64 + 32 + j * 4 + 2] = 255 * bit(x[r + 8], j)
+                        d.data[i * 512 + r * 64 + 32 + j * 4 + 3] = 255
+                    }
                 }
             }
+            ctx.putImageData(d, 0 + p % 1280, 100 + Math.floor(p / 1280) * 16)
         }
         for(let i = 0; i < 176; i++) {
-            sprite16(0x32ec + i * 32)
+            sprite16(0x32ec + i * 32, i * 16)
         }
-        let sprite8 = (o) => {
+
+        let sprite8 = (o, p) => {
+            let d = ctx.createImageData(8, 8)
             let x = new Uint8Array(this.data, o, 8)
             for(let r = 0; r < 8; r++) {
-                let s = x[r].toString(2).padStart(8, 0)
-                content += s.replace(/0/g, "  ").replace(/1/g, "##") + "\n"
+                for(let j = 0; j < 8; j++) {
+                    d.data[r * 8 * 4 + j * 4 + 0] = 255 * bit(x[r], j)
+                    d.data[r * 8 * 4 + j * 4 + 1] = 255 * bit(x[r], j)
+                    d.data[r * 8 * 4 + j * 4 + 2] = 255 * bit(x[r], j)
+                    d.data[r * 8 * 4 + j * 4 + 3] = 255
+                }
             }
+            ctx.putImageData(d, 100 + p, 200)
         }
         for(let i = 0; i < 21; i++) {
-            sprite8(0x4965 + i * 8)
+            sprite8(0x4965 + i * 8, i * 8)
         }
         this.out.textContent = content
     }
