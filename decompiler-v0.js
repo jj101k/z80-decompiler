d9300422f98bacc23361c05f326b7fffc3d74e04
Jim Driscoll <jim.a.driscoll@gmail.com>
2024-02-02 22:13:12 +0000

    Expand 8-bit LD support

--- decompiler-v0.js
+++ decompiler-v0.js
@@ -5,31 +5,61 @@ const contents = fs.readFileSync(filename)
 
 const loadPoint = 0x5e27 // From previous file
 
-const tryDecode = (n) => {
-    // 8-bit load group LD (40-7f excl. 76)
-    if(((n >> 6) & 0b11) == 0b01 && n != 0x76) {
-        const registerRef = {
-            [0b111]: "A",
-            [0b000]: "B",
-            [0b001]: "C",
-            [0b010]: "D",
-            [0b011]: "E",
-            [0b100]: "F",
-            [0b101]: "L",
-            [0b110]: "(HL)",
+const tryDecode = (n, o) => {
+    const registerRef = {
+        [0b111]: "A",
+        [0b000]: "B",
+        [0b001]: "C",
+        [0b010]: "D",
+        [0b011]: "E",
+        [0b100]: "F",
+        [0b101]: "L",
+        [0b110]: "(HL)",
+    }
+    // 8-bit load group LD (grouped cases)
+    if(n == 0xfd || n == 0xdd) {
+        const r1 = (n == 0xfd) ? "IY" : "IX"
+        const nn = contents[o + 1]
+        if((nn >> 3) == 0b1110 && (nn & 0b111) != 0b110) {
+            const r = nn & 0b111
+            let dv = new DataView(contents.buffer, o + 2, 1)
+            const d = dv.getInt8(0)
+            return {o: o + 3, n: `LD (${r1}+${d}), ${r}`}
+        } else if((nn & 0b1100_0111) == 0b01000110 && ((nn >> 3) & 0b111) != 0b110) {
+            const r = nn & 0b111
+            let dv = new DataView(contents.buffer, o + 2, 1)
+            const d = dv.getInt8(0)
+            return {o: o + 3, n: `LD ${r}, (${r1}+${d})`}
         }
+    } else if((n & 0b1100_0111) == 0b0000_0110 && (n & 0b11_1000) != 0b11_0000) {
+        const d = registerRef[(n >> 3) & 0b111]
+        const a = contents[o + 1]
+        return {o: o + 2, n: `LD ${d}, ${a.toString(16)}`}
+    } else if((n & 0b1100_0111) == 0b0100_0110 && (n & 0b11_0000) != 0b11_1000) {
+        const d = registerRef[(n >> 3) & 0b111]
+        return {o: o + 1, n: `LD ${d}, (HL)`}
+    } else if((n >> 3) == 0b1110 && (n & 0b111) != 0b110) {
+        const s = registerRef[n & 0b111]
+        return {o: o + 1, n: `LD (HL), ${s}`}
+    } else if(((n >> 6) & 0b11) == 0b01 && n != 0x76) {
         const s = registerRef[n & 0b111]
         const d = registerRef[(n >> 3) & 0b111]
-        return `LD ${d}, ${s}`
+        return {o: o + 1, n: `LD ${d}, ${s}`}
     }
+
+    return null
 }
 
 const codes = {
     [0x00]: {n: "NOP"},
     [0x01]: {a: "s", n: "LD BC, nn"},
+    [0x02]: {n: "LD (BC), A"},
     [0x06]: {a: "c", n: "LD B, n"},
+    [0x0a]: {n: "LD A, (BC)"},
     [0x11]: {a: "s", n: "LD DE, nn"},
+    [0x12]: {n: "LD (DE), A"},
     [0x16]: {a: "c", n: "LD D, n"},
+    [0x1a]: {n: "LD A, (DE)"},
     [0x20]: {a: "d", n: "JR NZ, d"},
     [0x21]: {a: "s", n: "LD HL, nn"},
     [0x31]: {a: "s", n: "LD SP, nn"},
@@ -49,7 +79,13 @@ const codes = {
     [0xcd]: {a: "s", n: "CALL nn"},
     [0xd3]: {a: "c", n: "OUT (n), A"},
     [0xd6]: {a: "c", n: "SUB n"},
+    [0xdd36]: {a: "c", n: "LD (IX+d), n"},
+    [0xed47]: {n: "LD I, A"},
+    [0xed4f]: {n: "LD R, A"},
+    [0xed57]: {n: "LD A, I"},
+    [0xed5f]: {n: "LD A, R"},
     [0xedb0]: {n: "LDIR"},
+    [0xfd36]: {a: "c", n: "LD (IY+d), n"},
     [0xf3]: {n: "DI"},
     [0xf5]: {n: "PUSH AF"},
     [0xfe]: {a: "c", n: "CP n"},
@@ -100,9 +136,9 @@ const l = (o2, {o, n}) => {
 }
 
 const decode = (o) => {
-    const n = tryDecode(contents[o])
-    if(n) {
-        return l(o, {o: o + 1, n})
+    const r = tryDecode(contents[o], o)
+    if(r) {
+        return l(o, r)
     }
     const c = codes[contents[o]]
     if(c) {
