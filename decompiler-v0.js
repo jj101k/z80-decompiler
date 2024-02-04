93931d5b6c95f6b7e81d0f3eee4687050c1e08ed
Jim Driscoll <jim.a.driscoll@gmail.com>
2024-02-04 12:01:01 +0000

    Doc update

--- decompiler-v0.js
+++ decompiler-v0.js
@@ -250,26 +250,26 @@ class FDDD {
         const nnx = new BitView(this.#dw.uint8())
 
         switch(nnx.pre) {
-            case 0b00: {
+            case 0b00: { // 0x.d 0-3
                 const r = addHlIxIy(nnx, this.offsetRegister)
                 if(r) {
                     return r
                 }
-                if(nnx.a3 == hlIndirect && nnx.b3 == hlIndirect) { // 0x*d36
-                    const d = this.#dw.int8()
-                    const n = this.#dw.uint8()
-                    return `LD (${this.offsetRegister}${rel(d)}), ${u8(n)}`
-                } else if(nnx.a3 == hlIndirect && (nnx.b3 & 0b110) == 0b100) { // 0x*d34-5
+                if(nnx.rest == 0b10_0001) { // 0x.d21
+                    const s = this.#dw.uint16()
+                    return `LD IX, ${u16(s)}`
+                } else if(nnx.a3 == hlIndirect && (nnx.b3 & 0b110) == 0b100) { // 0x.d34-5
                     const op = (nnx.b3 & 1) ? "DEC" : "INC"
                     const d = this.#dw.int8()
                     return `${op} (${this.offsetRegister}${rel(d)})`
-                } else if(nnx.rest == 0b10_0001) { // 0x*d21
-                    const s = this.#dw.uint16()
-                    return `LD IX, ${u16(s)}`
+                } else if(nnx.a3 == hlIndirect && nnx.b3 == hlIndirect) { // 0x.d36
+                    const d = this.#dw.int8()
+                    const n = this.#dw.uint8()
+                    return `LD (${this.offsetRegister}${rel(d)}), ${u8(n)}`
                 }
                 break
             }
-            case 0b01: {
+            case 0b01: { // 0x.d 4-7
                 if(nnx.a3 == hlIndirect && nnx.b3 != hlIndirect) {
                     const r = nnx.b3
                     const d = this.#dw.int8()
@@ -281,7 +281,7 @@ class FDDD {
                 }
                 break
             }
-            case 0b10: {
+            case 0b10: { // 0x.d 8-b
                 if(nnx.b3 == hlIndirect) {
                     const op = arOpR[nnx.a3]
                     const d = this.#dw.int8()
@@ -290,7 +290,7 @@ class FDDD {
                 }
                 break
             }
-            case 0b11: {
+            case 0b11: { // 0x.d c-f
                 if(nnx.rest == 0b00_1011) { // 0xcb
                     // Bit manipulation
                     const a = this.#dw.uint8()
