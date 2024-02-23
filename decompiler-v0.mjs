62a848706f5e54328303c86aa65a2d5a5d757789
Jim Driscoll <jim.a.driscoll@gmail.com>
2024-02-23 08:29:15 +0000

    Update to newer Node style - imports

--- decompiler-v0.mjs
+++ decompiler-v0.mjs
@@ -1,10 +1,10 @@
-const fs = require("fs")
+import { DataWalker } from "./lib/DataWalker.mjs"
+import { DecompileWalker } from "./lib/DecompileWalker.mjs"
+import fs from "fs"
+
 const [filename, loadPoint, startOffset] = process.argv.slice(2)
 
 console.log(`Reading ${filename}`)
-
-const DataWalker = require("./lib/DataWalker")
-const DecompileWalker = require("./lib/DecompileWalker")
 
 /**
  *
@@ -21,17 +21,17 @@ function decode(filename, loadPoint, startOffset) {
     let bytesParsed = 0
 
     try {
-        for(let i = 0; i < 10_000; i++) {
+        for (let i = 0; i < 10_000; i++) {
             const startPoint = dw.offset
             const n = decompile.decode()
-            if(!n) {
+            if (!n) {
                 dw.offset = startPoint
                 throw new Error(`Cannot decode value at offset ${decompile.addr(startPoint + loadPoint)} after ${i} points (${bytesParsed} bytes) mapped: ${decompile.u8(...dw.inspect())}`)
             }
             const byteLength = decompile.lastEndPoint - startPoint
             bytesParsed += byteLength
-            if(decompile.finished) {
-                for(const l of decompile.dump()) {
+            if (decompile.finished) {
+                for (const l of decompile.dump()) {
                     console.log(l)
                 }
                 console.log(`Stop after ${i} - nothing left to examine`)
@@ -40,7 +40,7 @@ function decode(filename, loadPoint, startOffset) {
         }
     } catch (e) {
         try {
-            for(const l of decompile.dump()) {
+            for (const l of decompile.dump()) {
                 console.log(l)
             }
         } finally {
